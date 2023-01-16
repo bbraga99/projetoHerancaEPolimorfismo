13680d3b8137b0f5f931e415825dcdcc89c955bd
Bruno Braga <brunuu.henrique@hotmail.com>
2023-01-16 19:59:13 -0300

    First commit

---
+++ ConsoleApp1/Entities/OutsourceEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    class OutsourceEmployee : Employee
+    {
+
+        public double AdditionalCharge { get; set; }
+
+        public OutsourceEmployee()
+        {
+        }
+
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name,hours,valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;    
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.10 * AdditionalCharge;
+        }
+
+    }
+}
